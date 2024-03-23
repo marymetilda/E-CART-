f38ec88a753ca4d5c37ae5a23ab7920a3c68a9f5
mary-metilda <mary.metilda@vonnue.com>
2024-03-23 12:41:43 +0530

    feat: update user profile api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   logoutCurrentUser,
   getAllUsers,
   getCurrentUserProfile,
+  updateCurrentUserProfile,
 } from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
@@ -17,6 +18,9 @@ router
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
-router.route("/profile").get(authenticate, getCurrentUserProfile);
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
 
 export default router;
