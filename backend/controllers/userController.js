f5babb534010d2c0e0154ca041ad33232368b524
mary-metilda <mary.metilda@vonnue.com>
2024-03-23 11:13:03 +0530

    feat: logout user api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,4 +57,13 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { createUser, loginUser };
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out successfully" });
+});
+
+export { createUser, loginUser, logoutCurrentUser };
