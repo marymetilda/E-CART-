340c0f7421fb41eb604d8a400ac4fcaff0923939
mary-metilda <mary.metilda@vonnue.com>
2024-04-06 11:02:02 +0530

    feat: create category api

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -1,13 +1,13 @@
-import mongoose from mongoose;
+import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true, 
-        required: true,
-        maxLength: 32,
-        unique: true
-    }
-})
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxLength: 32,
+    unique: true,
+  },
+});
 
-export default mongoose.model('Category', categorySchema)
+export default mongoose.model("Category", categorySchema);
