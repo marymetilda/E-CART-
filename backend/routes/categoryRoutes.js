d7343e197d94debae3187e8b8c11c94943866c1b
mary-metilda <mary.metilda@vonnue.com>
2024-04-06 11:37:44 +0530

    feat: update category api

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
-import { createCategory } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  updateCategory,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.route("/").post(authenticate, authorizeAdmin, createCategory);
+router.route("/:categoryId").put(authenticate, authorizeAdmin, updateCategory);
 
 export default router;
