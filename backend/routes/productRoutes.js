0805d4e96d9e72bb86b8f587e4179529d4e7ceec
mary-metilda <mary.metilda@vonnue.com>
2024-04-11 10:22:48 +0530

    feat: create product api

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,6 +2,12 @@ import express from "express";
 import formidable from "express-formidable";
 const router = express.Router();
 
+// controllers
+import { addProduct } from "../controllers/productController.js";
+
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
+
+router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct);
 
 export default router;
