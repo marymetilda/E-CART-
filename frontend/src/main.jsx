59baca8b68e49ffc51147fbd43e0f99e67d440cb
mary-metilda <mary.metilda@vonnue.com>
2024-04-18 15:44:39 +0530

    feat: create product list page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,6 +16,7 @@ import Profile from "./pages/User/Profile.jsx";
 import AdminRoute from "./pages/Admin/AdminRoute.jsx";
 import UserList from "./pages/Admin/UserList.jsx";
 import CategoryList from "./pages/Admin/CategoryList.jsx";
+import ProductList from "./pages/Admin/ProductList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -31,6 +32,7 @@ const router = createBrowserRouter(
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
         <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productList" element={<ProductList />} />
       </Route>
     </Route>
   )
