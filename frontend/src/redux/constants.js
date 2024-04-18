824c8a538eb7a5dca57abc69afa7270e938e1ff0
mary-metilda <mary.metilda@vonnue.com>
2024-04-18 09:41:00 +0530

    feat: set up for api endpoints frontend

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,7 @@
 const BASE_URL = "";
 const USERS_URL = "/api/users";
 const CATEGORY_URL = "/api/category";
+const PRODUCT_URL = "/api/products";
+const UPLOAD_URL = "/api/upload";
 
-export { BASE_URL, USERS_URL, CATEGORY_URL };
+export { BASE_URL, USERS_URL, CATEGORY_URL, PRODUCT_URL, UPLOAD_URL };
