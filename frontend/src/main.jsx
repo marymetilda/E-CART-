83b7a332c477bf5bd89cadb2d8ac7260e0306fc7
mary-metilda <mary.metilda@vonnue.com>
2024-04-19 15:41:31 +0530

    feat: all products page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,7 @@ import UserList from "./pages/Admin/UserList.jsx";
 import CategoryList from "./pages/Admin/CategoryList.jsx";
 import ProductList from "./pages/Admin/ProductList.jsx";
 import ProductUpdate from "./pages/Admin/ProductUpdate.jsx";
+import AllProducts from "./pages/Admin/AllProducts.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
         <Route path="userlist" element={<UserList />} />
         <Route path="categorylist" element={<CategoryList />} />
         <Route path="productList" element={<ProductList />} />
+        <Route path="allproductslist" element={<AllProducts />} />
         <Route path="product/update/:id" element={<ProductUpdate />} />
       </Route>
     </Route>
