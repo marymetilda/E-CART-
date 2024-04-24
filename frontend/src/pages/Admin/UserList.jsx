8d1b0cf849c25b4abd0c1abdda20e05d2c3c7aa1
mary-metilda <mary.metilda@vonnue.com>
2024-04-24 14:55:16 +0530

    feat: update product page

--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -8,6 +8,7 @@ import {
   useDeleteUserMutation,
   useUpdateUserMutation,
 } from "../../redux/api/usersApiSlice";
+import AdminMenu from "./AdminMenu";
 
 const UserList = () => {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
@@ -63,7 +64,7 @@ const UserList = () => {
         </Message>
       ) : (
         <div className="flex flex-col md:flex-row text-white">
-          {/** <AdminMenu /> */}
+          * <AdminMenu />
           <table className="w-full md:w-4/5 mx-auto">
             <thead>
               <tr>
