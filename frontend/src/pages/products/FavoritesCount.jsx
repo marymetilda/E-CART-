785020ad3a06679d1314bc8540068ab6aefbf4be
mary-metilda <mary.metilda@vonnue.com>
2024-04-25 14:17:41 +0530

    feat: favorites page

---
+++ frontend/src/pages/products/FavoritesCount.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+
+const FavoritesCount = () => {
+  const favorites = useSelector((state) => state.favorites);
+  const FavoriteCount = favorites.length;
+
+  return (
+    <div className="absolute left-2 top-8">
+      {FavoriteCount > 0 && (
+        <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+          {FavoriteCount}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default FavoritesCount;
