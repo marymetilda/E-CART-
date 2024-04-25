2d08b0f0da310a57292fe1e6bf2558d0b718fcfc
mary-metilda <mary.metilda@vonnue.com>
2024-04-25 13:11:34 +0530

    feat: heart icon component

---
+++ frontend/src/pages/products/HeartIcon.jsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addToFavorites,
+  removeFromFavorites,
+  setFavorites,
+} from "../../redux/features/favorites/favoriteSlice";
+import {
+  addFavoriteToLocalStorage,
+  getFavoritesFromLocalStorage,
+  removeFavoriteFromLocalStorage,
+} from "../../utils/localStorage";
+
+const HeartIcon = ({ product }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites) || [];
+  const isFavorites = favorites.some((p) => p._id === product._id);
+
+  useEffect(() => {
+    const favoritesFromLocalStorage = getFavoritesFromLocalStorage();
+    dispatch(setFavorites(favoritesFromLocalStorage));
+  }, []);
+
+  const toggleFavorites = () => {
+    if (isFavorites) {
+      dispatch(removeFromFavorites(product));
+      // remove the product from the localStorage as well
+      removeFavoriteFromLocalStorage(product._id);
+    } else {
+      dispatch(addToFavorites(product));
+      // add the product to localStorage as well
+      addFavoriteToLocalStorage(product);
+    }
+  };
+
+  return (
+    <div
+      onClick={toggleFavorites}
+      className="absolute top-2 right-5 cursor-pointer"
+    >
+      {isFavorites ? (
+        <FaHeart className="text-pink-500" />
+      ) : (
+        <FaRegHeart className="text-white" />
+      )}
+    </div>
+  );
+};
+
+export default HeartIcon;
