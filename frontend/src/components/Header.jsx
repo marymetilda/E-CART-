43bce8f3b15ed7aa5fd7d3ad6f3976be8d90d791
mary-metilda <mary.metilda@vonnue.com>
2024-04-25 11:18:16 +0530

    feat: header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,35 @@
+import ProductCarousel from "../pages/products/ProductCarousel";
+import SmallProduct from "../pages/products/SmallProduct";
+import { useGetTopProductsQuery } from "../redux/api/productApiSlice";
+import Loader from "./Loader";
+
 const Header = () => {
-  return <div>Header</div>;
+  const { data, isLoading, isError } = useGetTopProductsQuery();
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <h1>ERROR</h1>;
+  }
+
+  return (
+    <>
+      <div className="flex justify-around">
+        <div className="xl:block lg:hidden md:hidden sm:hidden">
+          <div className="grid grid-cols-2">
+            {data.map((product) => (
+              <div key={product._id}>
+                <SmallProduct product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <ProductCarousel />
+      </div>
+    </>
+  );
 };
 
 export default Header;
