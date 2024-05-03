b941132141368a52176ff18b31d9afb3cd8623d4
mary-metilda <mary.metilda@vonnue.com>
2024-05-03 12:32:22 +0530

    feat: cart page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { Link, useParams } from "react-router-dom";
-import { useGetProductsQuery } from "./redux/api/productApiSlice";
-import Loader from "./components/Loader";
-import Message from "./components/Message";
-import Header from "./components/Header";
-import Product from "./pages/products/Product";
+import { useGetProductsQuery } from "../redux/api/productApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Header from "../components/Header";
+import Product from "./products/Product";
 
 const Home = () => {
   const { keyword } = useParams();
