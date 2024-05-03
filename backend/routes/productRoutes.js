ac62cdc7d3702c5c8bb48787b529e26639c8ad3f
mary-metilda <mary.metilda@vonnue.com>
2024-05-03 13:07:09 +0530

    feat: add backend for the filtered products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,6 +13,7 @@ import {
   updateProductDetails,
   fetchTopProducts,
   fetchNewProducts,
+  filterProducts,
 } from "../controllers/productController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
@@ -33,5 +34,7 @@ router
   .get(fetchProductById)
   .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
   .delete(authenticate, authorizeAdmin, removeProduct);
+
+router.route("/filtered-products").post(filterProducts);
 
 export default router;
