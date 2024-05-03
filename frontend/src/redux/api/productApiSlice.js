6c22a1f888cb5594b30a3b08b1993cc5ecd75b07
mary-metilda <mary.metilda@vonnue.com>
2024-05-03 13:11:27 +0530

    feat: add use get filtered product query

--- frontend/src/redux/api/productApiSlice.js
+++ frontend/src/redux/api/productApiSlice.js
@@ -83,6 +83,14 @@ export const productApiSlice = apiSlice.injectEndpoints({
       query: () => `${PRODUCT_URL}/new`,
       keepUnusedDataFor: 5,
     }),
+
+    getFilteredProducts: builder.query({
+      query: ({ checked, radio }) => ({
+        url: `${PRODUCT_URL}/filtered-products`,
+        method: "POST",
+        body: { checked, radio },
+      }),
+    }),
   }),
 });
 
@@ -98,4 +106,5 @@ export const {
   useGetTopProductsQuery,
   useGetNewProductsQuery,
   useUploadProductImageMutation,
+  useGetFilteredProductsQuery,
 } = productApiSlice;
