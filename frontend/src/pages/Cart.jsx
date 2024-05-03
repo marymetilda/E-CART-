81e00981ceaf7cc004ada85d26ccb3f725256a11
mary-metilda <mary.metilda@vonnue.com>
2024-05-03 12:45:18 +0530

    feat: add cart items in navigation

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -88,7 +88,11 @@ const Cart = () => {
               <div className="mt-8 w-[40rem]">
                 <div className="p-4 rounded-lg">
                   <h2 className="text-xl font-semibold mb-2">
-                    Items ({cartItems.reduce((acc, item) => acc + item.qty, 0)}{" "}
+                    Items (
+                    {cartItems.reduce(
+                      (acc, item) => acc + parseFloat(item.qty),
+                      0
+                    )}
                     )
                   </h2>
 
