440d071a6987e32c685b55ff2e0f053ca698c6a5
mary-metilda <mary.metilda@vonnue.com>
2024-05-03 10:32:58 +0530

    feat: create cart slice actions

--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../../../utils/cart";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
@@ -19,6 +20,40 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
+      return updateCart(state, item);
+    },
+
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+      return updateCart(state);
     },
   },
+
+  saveShippingAddress: (state, action) => {
+    state.shippingAddress = action.payload;
+    localStorage.setItem("cart", JSON.stringify(state));
+  },
+
+  savePaymentMethod: (state, action) => {
+    state.paymentMethod = action.payload;
+    localStorage.setItem("cart", JSON.stringify(state));
+  },
+
+  clearCartItems: (state) => {
+    state.cartItems = [];
+    localStorage.setItem("cart", JSON.stringify(state));
+  },
+
+  resetCart: (state) => (state = initialState),
 });
+
+export const {
+  addToCart,
+  removeFromCart,
+  savePaymentMethod,
+  saveShippingAddress,
+  clearCartItems,
+  resetCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
