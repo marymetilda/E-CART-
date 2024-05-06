8f7caf8283c42a14cd46cf37567967d22a24cd56
mary-metilda <mary.metilda@vonnue.com>
2024-05-06 17:37:15 +0530

    feat: add order api slice in redux

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -3,5 +3,15 @@ const USERS_URL = "/api/users";
 const CATEGORY_URL = "/api/category";
 const PRODUCT_URL = "/api/products";
 const UPLOAD_URL = "/api/upload/";
+const ORDERS_URL = "/api/orders";
+const PAYPAL_URL = "/api/config/paypal";
 
-export { BASE_URL, USERS_URL, CATEGORY_URL, PRODUCT_URL, UPLOAD_URL };
+export {
+  BASE_URL,
+  USERS_URL,
+  CATEGORY_URL,
+  PRODUCT_URL,
+  UPLOAD_URL,
+  ORDERS_URL,
+  PAYPAL_URL,
+};
