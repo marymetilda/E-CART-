33f5871a55bfb662732cc8cecf7b8c31de164f84
mary-metilda <mary.metilda@vonnue.com>
2024-05-13 17:12:54 +0530

    feat: admin dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -31,6 +31,7 @@ import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import Order from "./pages/Orders/Order.jsx";
 import UserOrder from "./pages/User/UserOrder.jsx";
 import OrderList from "./pages/Admin/OrderList.jsx";
+import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -59,6 +60,7 @@ const router = createBrowserRouter(
         <Route path="allproductslist" element={<AllProducts />} />
         <Route path="orderlist" element={<OrderList />} />
         <Route path="product/update/:id" element={<ProductUpdate />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
       </Route>
     </Route>
   )
