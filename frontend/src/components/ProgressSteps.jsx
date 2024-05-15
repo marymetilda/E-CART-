dba3fd76fd52c20499b0eae00400fc9f8c41697e
mary-metilda <mary.metilda@vonnue.com>
2024-05-15 18:06:58 +0530

    feat: progress steps mobile view

--- frontend/src/components/ProgressSteps.jsx
+++ frontend/src/components/ProgressSteps.jsx
@@ -1,14 +1,20 @@
 const ProgressSteps = ({ step1, step2, step3 }) => {
   return (
-    <div className="flex justify-center items-center space-x-4">
-      <div className={`${step1 ? "text-green-500" : "text-gray-300"}`}>
+    <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-x-4">
+      <div
+        className={`${step1 ? "text-green-500 flex items-center justify-center gap-4 lg:gap-0 lg:flex-col" : "text-gray-300"}`}
+      >
         <span className="ml-2">Login</span>
         <div className="mt-2 text-lg text-center">✅</div>
       </div>
       {step2 && (
         <>
-          {step1 && <div className="h-0.5 w-[10rem] bg-green-500"></div>}
-          <div className={`${step1 ? "text-green-500" : "text-gray-300"}`}>
+          {step1 && (
+            <div className="h-0.5 w-[10rem] bg-green-500 hidden lg:block"></div>
+          )}
+          <div
+            className={`${step1 ? "text-green-500 flex items-center justify-center gap-4 lg:gap-0 lg:flex-col" : "text-gray-300"}`}
+          >
             <span>Shipping</span>
             <div className="mt-2 text-lg text-center">✅</div>
           </div>
@@ -17,19 +23,23 @@ const ProgressSteps = ({ step1, step2, step3 }) => {
 
       <>
         {step1 && step2 && step3 ? (
-          <div className="h-0.5 w-[10rem] bg-green-500"></div>
+          <div className="hidden lg:block h-0.5 w-[10rem] bg-green-500"></div>
         ) : (
           ""
         )}
 
-        <div className={`${step3 ? "text-green-500" : "text-gray-300"}`}>
-          <span className={`${!step3 ? "ml-[10rem]" : ""}`}>Summary</span>
+        <>
+          <span
+            className={`${!step3 ? "lg:ml-[10rem] text-gray-300 flex items-center justify-center gap-4 lg:gap-0 lg:flex-col" : "text-green-500"}`}
+          >
+            Summary
+          </span>
           {step1 && step2 && step3 ? (
             <div className="mt-2 text-lg text-center">✅</div>
           ) : (
             ""
           )}
-        </div>
+        </>
       </>
     </div>
   );
