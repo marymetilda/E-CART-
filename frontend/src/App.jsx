c78dcea77662be13b44f9593ea58c3cc8395bdfe
mary-metilda <mary.metilda@vonnue.com>
2024-05-15 14:27:48 +0530

    feat: header mobile modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,21 @@ import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Auth/Navigation";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className="bg-black min-h-screen">
+    <div
+      className={`bg-black min-h-screen max-h-screen ${isModalOpen ? "overflow-hidden" : "overflow-scroll"}`}
+    >
       <ToastContainer />
-      <Navigation />
+      <Navigation handleModal={handleModal} />
       <main className="py-3 px-6">
         <Outlet />
       </main>
