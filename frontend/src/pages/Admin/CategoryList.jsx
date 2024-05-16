e44aae191b625964929932b019d5a4ab5d515169
mary-metilda <mary.metilda@vonnue.com>
2024-05-16 14:11:48 +0530

    feat: all product page mobile view

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -90,7 +90,7 @@ const CategoryList = () => {
   };
 
   return (
-    <div className="ml-[10rem] flex flex-col md:flex-row">
+    <div className="lg:pl-[6vw] flex flex-col md:flex-row">
       <AdminMenu />
       <div className="md:w-3/4 p-3">
         <div className="h-12">Manage Categories</div>
