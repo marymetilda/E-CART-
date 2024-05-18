02c9b5d75fcc1f5f13744f96bbf60553f5a30893
mary-metilda <mary.metilda@vonnue.com>
2024-05-18 14:43:19 +0530

    feat: update place order page theme

--- frontend/src/pages/Orders/PlaceOrder.jsx
+++ frontend/src/pages/Orders/PlaceOrder.jsx
@@ -44,46 +44,70 @@ const PlaceOrder = () => {
 
   return (
     <>
+      <div className="w-full pl-[6vw] lg:py-4">
+        <Link to="/">
+          <p className="text-[32px] font-bold tracking-wider text-center lg:text-left">
+            INFINITY
+          </p>
+        </Link>
+        <p className="text-center lg:text-left text-sm">
+          Give you infinite meanings in a limited amount of time!
+        </p>
+      </div>
+      <h2 className="text-4xl font-semibold mb-5 w-full text-center pb-10">
+        Order Summary
+      </h2>
       <ProgressSteps step1 step2 step3 />
 
-      <div className="container mx-auto mt-8 lg:pl-24">
+      <div className="container mx-auto mt-20 lg:pl-[5vw]">
         {cart.cartItems.length === 0 ? (
           <Message>Your cart is empty</Message>
         ) : (
-          <div className="overflow-x-auto">
+          <div className="overflow-x-auto lg:pr-8">
             <table className="w-full border-collapse">
               <thead>
                 <tr>
-                  <td className="px-1 py-2 text-left align-top">Image</td>
-                  <td className="px-1 py-2 text-left">Product</td>
-                  <td className="px-1 py-2 text-left">Quantity</td>
-                  <td className="px-1 py-2 text-left">Price</td>
-                  <td className="px-1 py-2 text-left">Total</td>
+                  <td className="px-1 py-2 text-sm sm:text-base text-center align-top bg-gradient-to-l from-slate-900 via-slate-600 to-slate-900">
+                    Image
+                  </td>
+                  <td className="px-1 py-2 text-sm sm:text-base text-center bg-gradient-to-l from-slate-900 via-slate-600 to-slate-900">
+                    Product
+                  </td>
+                  <td className="px-1 py-2 text-sm sm:text-base text-center bg-gradient-to-l from-slate-900 via-slate-600 to-slate-900">
+                    Quantity
+                  </td>
+                  <td className="px-1 py-2 text-sm sm:text-base text-center bg-gradient-to-l from-slate-900 via-slate-600 to-slate-900">
+                    Price
+                  </td>
+                  <td className="px-1 py-2 text-sm sm:text-base text-center bg-gradient-to-l from-slate-900 via-slate-600 to-slate-900">
+                    Total
+                  </td>
                 </tr>
               </thead>
 
               <tbody>
                 {cart.cartItems.map((item, index) => (
                   <tr key={index}>
-                    <td
-                      className="p2
-                        "
-                    >
+                    <td className="p-2 flex items-center justify-center bg-gradient-to-r from-slate-900 via-slate-700 to-slate-600 border-y-2 border-y-slate-900">
                       <img
                         src={item.image}
                         alt={item.image}
-                        className="w-16 h-16 object-cover"
+                        className="w-12 sm:w-16 h-12 sm:h-16 object-cover"
                       />
                     </td>
 
-                    <td className="p-2">
+                    <td className="p-2 text-sm sm:text-base text-center bg-gradient-to-r from-slate-900 via-slate-700 to-slate-600 border-y-2 border-y-slate-900">
                       <Link to={`/product/${item.product}`}>{item.name}</Link>
                     </td>
 
-                    <td className="p-2">{item.qty}</td>
-                    <td className="p-2">{item.price.toFixed(2)}</td>
-                    <td className="p-2">
-                      $ {(item.qty * item.price).toFixed(2)}
+                    <td className="p-2 text-sm sm:text-base text-center bg-gradient-to-r from-slate-900 via-slate-700 to-slate-600 border-y-2 border-y-slate-900">
+                      {item.qty}
+                    </td>
+                    <td className="p-2 text-sm sm:text-base text-center bg-gradient-to-r from-slate-900 via-slate-700 to-slate-600 border-y-2 border-y-slate-900">
+                      ₹ {item.price.toFixed(2)}
+                    </td>
+                    <td className="p-2 text-sm sm:text-base text-center bg-gradient-to-r from-slate-900 via-slate-700 to-slate-600 border-y-2 border-y-slate-900">
+                      ₹ {(item.qty * item.price).toFixed(2)}
                     </td>
                   </tr>
                 ))}
@@ -93,8 +117,7 @@ const PlaceOrder = () => {
         )}
 
         <div className="mt-8">
-          <h2 className="text-2xl font-semibold mb-5">Order Summary</h2>
-          <div className="flex justify-between gap-8 flex-wrap p-8 bg-[#181818]">
+          <div className="flex justify-between gap-8 flex-wrap p-8 lg:mr-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-900 via-slate-600 to-slate-900">
             <ul className="text-lg">
               <li>
                 <span className="font-semibold mb-4">Items:</span> ${" "}
@@ -132,16 +155,17 @@ const PlaceOrder = () => {
               </p>
             </div>
           </div>
-
-          <button
-            type="button"
-            className="bg-sky-500 text-white py-2 px-4 rounded-full text-lg w-full mt-4"
-            disabled={cart.cartItems === 0}
-            onClick={placeOrderHandler}
-          >
-            Place Order
-          </button>
-          {isLoading && <Loader />}
+          <div className="lg:px-80 py-10">
+            <button
+              type="button"
+              className="text-white py-2 px-4 rounded-full text-lg w-full mt-4 bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-slate-500 via-slate-900 to-slate-500"
+              disabled={cart.cartItems === 0}
+              onClick={placeOrderHandler}
+            >
+              Place Order
+            </button>
+            {isLoading && <Loader />}
+          </div>
         </div>
       </div>
     </>
