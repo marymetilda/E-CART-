1e7adb5a710133d444c39676d7b0963738467300
mary-metilda <mary.metilda@vonnue.com>
2024-05-23 12:45:16 +0530

    feat: add new products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,9 +3,12 @@ import ProductCarousel from "../pages/products/ProductCarousel";
 import SmallProduct from "../pages/products/SmallProduct";
 import Loader from "./Loader";
 import Marquee from "./Marquee";
+import LogoContainer from "./LogoContainer";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const { data, isLoading, isError } = useGetTopProductsQuery();
+  const { userInfo } = useSelector((state) => state.auth);
 
   if (isLoading) {
     return <Loader />;
@@ -17,15 +20,15 @@ const Header = () => {
 
   return (
     <>
-      <div className="w-full pl-[6vw] lg:py-4">
-        <p className="text-[32px] font-bold tracking-wider text-center lg:text-left">
-          INFINITY
-        </p>
-        <p className="text-center lg:text-left text-sm">
-          Give you infinite meanings in a limited amount of time!
-        </p>
-      </div>
+      <LogoContainer className="w-full pl-[6vw] lg:py-4" />
       <Marquee />
+      {userInfo ? (
+        <div className="text-center">
+          Welcome {userInfo.username}... Here you go!!!
+        </div>
+      ) : (
+        <div className="text-center">Sign in to explore more in INFINITY</div>
+      )}
       <div className="flex justify-around gap-4 pr-4 pl-[2vw] relative">
         <div className="grid-cols-2 hidden xl:grid relative">
           {data.map((product) => (
