fca0df552293114d86753151193971c5a028e003
mary-metilda <mary.metilda@vonnue.com>
2025-02-28 10:28:59 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/backend/dist")));
+// app.use(express.static(path.join(__dirname, "/backend/dist")));
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
